54dbb2c567a0cdd620c11aa0bc1e04703744f48f
AS <AS@ASD>
2021-01-02 05:43:22 +0530

    Changes As per Comment

--- GitDemoProject1/src/Test2.java
+++ GitDemoProject1/src/Test2.java
@@ -4,7 +4,9 @@ public class Test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Java Code Added By Developer......................");
-
-	}
+		System.out.println("Make Changes as Suggested");
+			
+		}
+	
 
 }
